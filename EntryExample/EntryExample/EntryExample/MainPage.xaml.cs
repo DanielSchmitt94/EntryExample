8987f22a2f7cc9f1dff8a0169b1e76ba1cd7d8cc
DanielSchmitt94 <40561004+DanielSchmitt94@users.noreply.github.com>
2019-05-21 08:59:56 +0200

    Initial commit

---
+++ EntryExample/EntryExample/EntryExample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace EntryExample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/visual/material-visual");
+        }
+    }
+}
